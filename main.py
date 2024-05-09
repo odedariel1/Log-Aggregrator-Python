2198d4ac0ce5037b0b244ee5334d2c3193fe7afd
oded.ariel <odedariel200@gmail.com>
2024-05-09 16:31:02 +0300

    Log Message Producer (done) RabbitMQ Integration (done) Log Message Processor (done) File Writing (done) Monitoring and Dashboard (ON WORK)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def dashboard():
         except Exception as e:
             app.logger.error(f"Failed to read {log_file}: {e}")
             abort(500, description=f"Error reading log file: {status}")
-    return render_template('dashboard.html',data={1,2,3}, log_counts=log_counts)
+    return render_template('dashboard.html', log_counts=log_counts)
 
 if __name__ == '__main__':
     app.run(debug=True)
