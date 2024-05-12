d67b49d5a93dc6e69f3fa7f64014a3f455d31889
oded.ariel <odedariel200@gmail.com>
2024-05-12 15:22:31 +0300

    Log Message Producer (done) RabbitMQ Integration (done) Log Message Processor (done) File Writing (done) Monitoring and Dashboard (done)

--- log_consumer.py
+++ log_consumer.py
@@ -6,7 +6,7 @@ def on_message_received(ch, method, properties, body):
     log = json.loads(body)
     log_file = f"Logs/{log['status']}_logs.txt"
     with open(log_file,'a') as file:
-        file.write(f"{log['timestamp']}-{log['message']}\n")
+        file.write(f"{log['timestamp']};{log['message']}\n")
         print(f"Processed log {log['status']}:{log['message']}")
 
 
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 from flask import Flask, render_template, abort
-
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/')
-def dashboard():
-    log_counts = {}
-    log_messages = {}
-    log_dates = {}
+
+def read_data(file_name):
+    with open(file_name) as f:
+        yield from f
+
+
+def count_lines(file_name):
+    with open(file_name) as f:
+        return len(f.readlines())
+
+
+def file_reader(log_counts, log_data):
     for status in ['info', 'warning', 'error']:
+        log_counts[status] = 0
+        log_data[status] = []
         log_file = f"Logs/{status}_logs.txt"
         try:
-            with open(log_file, 'r') as file:
-                log_counts[status] = len(file.readlines())
+            data = read_data(log_file)
+            log_counts[status] = count_lines(log_file)
+            for data_list in range(log_counts[status]):
+                content = next(data).split(";")
+                content[0] = datetime.strptime(content[0], '%Y-%m-%dT%H:%M:%S.%f')
+                log_data[status].append(content)
 
+        except StopIteration:
+            continue
         except FileNotFoundError:
             log_counts[status] = 0
         except Exception as e:
             app.logger.error(f"Failed to read {log_file}: {e}")
             abort(500, description=f"Error reading log file: {status}")
-    return render_template('dashboard.html', log_counts=log_counts)
+
+
+@app.route('/')
+def dashboard():
+    log_counts = {}
+    log_data = {}
+    file_reader(log_counts, log_data)
+    return render_template('dashboard.html', log_data=log_data, log_counts=log_counts)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
